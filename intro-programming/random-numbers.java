4caeff0dbb33c6ee9c2bf43a83433a218d7b2b86
Jon Godinez <43837087+jongodinez@users.noreply.github.com>
2020-03-25 21:28:18 -0700

    removed comments

--- intro-programming/random-numbers.java
+++ intro-programming/random-numbers.java
@@ -2,10 +2,8 @@ import java.util.Random;
 
 class randomNumbers {
     public static void main(String[] args) {
-    // what is our range?
     int max = 100;
     int min = 1;
-  // create instance of Random class
     Random randomNum = new Random();
     int showMe = min + randomNum.nextInt(max);
     System.out.println(showMe);
