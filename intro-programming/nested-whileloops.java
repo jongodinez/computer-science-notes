e8b5c84f68f110c66a81f005b25876a946761f2d
Jon Godinez <43837087+jongodinez@users.noreply.github.com>
2020-03-29 11:25:38 -0700

    Create nested-whileloops.java

---
+++ intro-programming/nested-whileloops.java
@@ -0,0 +1,38 @@
+// class nestedWhileLoop
+// {
+//     public static void main(String[] args) 
+//     {
+//         int outerLoop = 2;
+//             while(outerLoop < 5)
+//             {
+//                 int innerLoop = 4;
+//                 while(innerLoop < 7)
+//                 {
+//                     System.out.println(outerLoop + ":" + innerLoop);
+//                     innerLoop++;
+//                 }
+//                 outerLoop++;
+//             }
+//     }
+// }
+
+class nestedWhileLoop
+{
+    public static void main(String[] args) {
+        int outerLoop = 3;
+        while (outerLoop < 8) {
+            int innerLoop = 5;
+            while (innerLoop < 7)
+
+                if {
+                (innerLoop == 6)
+                break;
+                    } else {
+                        {
+                        System.out.println(outerloop + ":" + innerLoop);
+                        innerLoop++}
+                    }
+                outerLoop++    
+        }
+    }
+}
